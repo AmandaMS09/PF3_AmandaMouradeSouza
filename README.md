66383c0c979a908cd6d181f0e42c8168bd28f9e7
AmandaMS09 <mandanevs@gmail.com>
2021-11-22 10:15:41 -0300

    git commit -m "Finalização do projeto

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
-# poupanca
-Prova técnica Final da matéria de TPA. Site de projeção de poupança feito com as tecnologias HTML, CSS e JavaScript.
+# <a href="https://amandams09.github.io/PF3_AmandaMouradeSouza/>Poupanca</a>
+Site de projeção de poupança que calcula o valor total que seria poupado no intervalo de tempo inserido pelo usuário.
+Feito com as tecnologias HTML, CSS e JavaScript.
--- js/script.js
+++ js/script.js
@@ -1,3 +1,23 @@
-//window.alert('Olá mundo')
-/*window.confirm('Você é legal?')
-window.prompt('Qual é o seu nome?')*/
+const calcular = document.getElementById('calcular');
+
+function calculo(){
+      let nome = document.getElementById("nome").value;
+      let P = document.getElementById("P").value;
+      let i = document.getElementById("i").value / 100;
+      let n = document.getElementById("n").value;
+
+      let vf = (P * (((1 + i) ** n - 1) / i)).toFixed(2);  
+
+    if (nome !="" && P !="" && i !="" && n !="")
+    {
+        var result = `Olá, ${nome}! Ao aplicar um valor de R$ ${P} mensais em um intervalo de ${n} meses a uma taxa de ${i * 100}% ao mês, você acumulará uma poupança de ${vf}`;
+      
+    }else
+    {
+        var result = "Preencha todos os campos!"
+    }
+    
+    document.getElementById("resultado").innerHTML = result;
+}
+
+calcular.addEventListener('click',calculo);
